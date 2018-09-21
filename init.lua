ed54c8bccd5b98b8415f795a778e170a2f7673e4
Kyle Mills <khonkhortisan@gmail.com>
2018-09-21 01:34:07 -0700

    init.lua from wiki

---
+++ init.lua
@@ -0,0 +1,22 @@
+-- Multiplayer Hack
+-- tome-multiplayer/init.lua
+
+long_name = "Multiplayer Hack"
+short_name = "multiplayer"
+for_module = "tome"
+version = {1,5,10}
+addon_version = {1,0,0}
+weight = 100
+author = {'Khonkhortisan'}
+homepage = 'https://github.com/khonkhortisan'
+description = [[Todo:
+Adds a second character with placeholder AI that on its turn gives control to the player and takes over the first character
+New game menu allowing different classes/races
+Game rebalance: Duplicates item pickups,quest rewards,etc/disallows dropping items
+transmog popup for both people]]
+tags = {'multiplayer'}
+
+overload = true
+superload = false
+data = true
+hooks = true
