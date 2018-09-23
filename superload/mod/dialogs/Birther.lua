a556976e237aac4742a4669258731c87ff52c409
Kyle Mills <khonkhortisan@gmail.com>
2018-09-23 15:59:09 -0700

    poof extra player

--- superload/mod/dialogs/Birther.lua
+++ superload/mod/dialogs/Birther.lua
@@ -14,8 +14,20 @@ function _M:init(title, actor, order, at_end, quickbirth, w, h)
 	self.c_cancel.fct=function()
 		if config.settings.multiplayer_num > 1 then
 			game:unregisterDialog(self)
+			
 			--self.player.explode
+			game.player.exp_worth = 0
+			game.player.on_die = nil
+			game.player.die = nil
+			game.player:die()
+			game.party:removeMember(game.player, true)
+			
 			--or turn into temporal clone
+			--data/talents/chronomancy/anomaly.lua anomaly evil twin
+			--data/talents/chronomancy/chronomancer makeparadoxclone
+			if game.party and game.party:findMember{main=true} then
+			--game.party:findMember{main=true}:forceUseTalent(self.T_ANOMALY_EVIL_TWIN, {ignore_energy=true})
+			end
 		else
 			self:atEnd("quit")
 		end
