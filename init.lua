70e119692cbbfeb3ea54cdc361a63dcd0c9c4741
Kyle Mills <khonkhortisan@gmail.com>
2018-09-22 19:52:44 -0700

    todo

--- init.lua
+++ init.lua
@@ -9,11 +9,19 @@ addon_version = {1,0,0}
 weight = 100
 author = {'Khonkhortisan'}
 homepage = 'https://github.com/khonkhortisan'
-description = [[Todo:
-Adds a second character with placeholder AI that on its turn gives control to the player and takes over the first character
-New game menu allowing different classes/races
-Game rebalance (optional): Duplicates item pickups,quest rewards,etc/disallows dropping items
-transmog popup for both people]]
+description = [[Adds a second character
+New game menu allowing different classes/races (after starting level)
+Todo:
+placeholder AI that on its turn gives control to the player and takes over the first character
+Game rebalance (optional):
+	duplicates starting items (transmog, rod of recall)
+	Duplicates item pickups
+	duplicates quest rewards
+	shared experience points
+	quest management only on player1?
+	disallows dropping items?
+transmog popup for both people
+bring both back to life after eidolon]]
 tags = {'multiplayer'}
 
 overload = true
