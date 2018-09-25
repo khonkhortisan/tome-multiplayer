1c4fd8049aef46a1ebf611a695260495b1117891
Kyle Mills <khonkhortisan@gmail.com>
2018-09-25 14:29:00 -0700

    STOP WASTING TURNS ON PLAYER SWITCH stop spending energy, stop adding extra energy, stop infinitely looping on trying to fix this, stop waiting until the second turn to be able to act fixes #13

--- overload/mod/ai/party.lua
+++ overload/mod/ai/party.lua
@@ -1,5 +1,5 @@
-local Player = require "mod.class.Player"
-local Party = require "mod.class.Party"
+--local Player = require "mod.class.Player"
+--local Party = require "mod.class.Party"
 --[[
 
 local _M = loadPrevious(...)
@@ -36,8 +36,9 @@ return _M
 -- darkgod@te4.org
 
 newAI("party_member", function(self)
+local oldp, newp = game.player, self
 --stop controlling golems and letting Norgan act multiple times
-if game.party.members[self].title == "Main character" then
+if game.party.members[newp].title == "Main character" then
 --game.party means enemies don't also form parties?
 --if true then
 	--game.player.ai = "party_member"
@@ -63,34 +64,72 @@ if game.party.members[self].title == "Main character" then
 	--game.player._runStop = game.player.runStop
 	--game.player._restStop = game.player.restStop
 	
-	--save player1's resting state
-	game.player.resting_continue=game.player.resting
-	game.player.running_continue=game.player.running
+	--THROW items
+	--[[
+	local chest = self.zone:makeEntityByName(self.level, "object", "TRANSMO_CHEST")
+			if chest then
+				self.zone:addEntity(self.level, chest, "object")
+				self.player:addObject(self.player:getInven("INVEN"), chest)
+			end
+	local orb = game.zone:makeEntityByName(game.level, "object", "ORB_SCRYING")
+			if orb then player:addObject(player:getInven("INVEN"), orb) orb:added() orb:identify(true) end
+	--]]
+	--local chest = game.zone:getEntityByName(oldp.level, "object", "TRANSMO_CHEST")
+	--local orb = game.zone:getEntityByName(oldp.level, "object", "ORB_SCRYING")
 	
-	game.party:setPlayer(self) --forcing handoff takes a turn?
+	--[[
+	local chest, chest_item = oldp:findInInventory(oldp:getInven("INVEN"), "Transmogrification Chest")
+	local orb, orb_item = oldp:findInInventory(oldp:getInven("INVEN"), "Scrying Orb")
+	newp:addObject(newp:getInven("INVEN"), chest)
+	newp:addObject(newp:getInven("INVEN"), orb)
+	--]]
+	
+	
+	--if it's actually my turn
+	--if (not game.player:enoughEnergy()) and self:enoughEnergy() then
+	--if newp:enoughEnergy() then
+	--save player1's resting state
+	oldp.resting_continue=oldp.resting
+	oldp.running_continue=oldp.running
+	--these are arrays containing functions
+	
+	--local energy_freeze1,energy_freeze2 = oldp.energy.value, newp.energy.value
+	
+	--forcing handoff takes a turn? 
+	game.party:setPlayer(newp) --game.player: oldp→newp
+	--mod/class/Party.lua:setPlayer calls mod/class/Actor.lua:move with force=true which doesn't call self:useEnergy
+	
+	--game.paused = true
+	
+	--This can't be right.
+	--self.energy.value = self.energy.value + game.energy_to_act
+	--oldp.energy.value, newp.energy.value = energy_freeze1, energy_freeze2
+	--from mod/class/NPC.lua
+	-- If AI did nothing, use energy anyway
+		--if not self.energy.used then self:useEnergy() end
+		--if old_energy == self.energy.value then break end -- Prevent infinite loops
+	self.energy.used = true --STOP USING UP TURNS ON PLAYER SWITCH
 	
 	--restore player2's resting state
-	if game.player.resting_continue then
-		game.player.resting =	game.player.resting_continue
-			game.player.resting_continue = false
-		--game.player:restInit()
-		game.player:restStep()
+	if newp.resting_continue then
+		newp.resting =	newp.resting_continue
+			newp.resting_continue = false
+		--newp:restInit()
+		newp:restStep()
 	end
-	if game.player.running_continue then
-		--local dir = game.player.running_continue.dir
-		game.player.running =	game.player.running_continue
-			game.player.running_continue = false
-		--game.player.runInit(dir)
-		game.player:runStep()
+	if newp.running_continue then
+		--local dir = newp.running_continue.dir
+		newp.running =	newp.running_continue
+			newp.running_continue = false
+		--newp.runInit(dir)
+		newp:runStep()
 	end
 	
 	--game.player.runStop = game.player._runStop
 	--game.player.restStop = game.player._restStop
 	--currentplayer.ai = "party_member"
 	--game.turn = game.turn - 9 --...so turns can go backward and (turn numbers) don't matter.
-	
-	--This can't be right.
-	self.energy.value = self.energy.value + game.energy_to_act
+	--end
 	
 	--what does the return value mean?
 	return
