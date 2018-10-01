khonkhortisan/tome-multiplayer

commit 0a6a041bbc6a7b10a41c0efd4bdb34c1ba91e008

fix swapped player names AND save name birther loads player1's name without player number suffix

superload/mod/class/Game.lua
@@ -195,7 +195,7 @@ Dialog:yesnoPopup("Multiplayer", "Ready Player "..config.settings.multiplayer_nu
 				--local player1 = self.player
 				local x, y = util.findFreeGrid(self.player.x, self.player.y, 20, true, {[engine.Map.ACTOR]=true})
 				
-				local player2 = Player.new{name=self.player_name.."2", game_ender=true}
+				local player2 = Player.new{name=self.player_name..config.settings.multiplayer_num, game_ender=true}
 				
 				--actually spawn player2 (Norgan-style!) - required for setPlayer
 				self.zone:addEntity(self.level, player2, "actor", x, y)
@@ -307,6 +307,28 @@ end
 --perplayer character creation dialog
 	self:registerDialog(birth)
 end
+
+local base_setPlayerName = _M.setPlayerName
+--- Sets the player name
+function _M:setPlayerName(name)
+	name = name:removeColorCodes():gsub("#", " "):sub(1, 25)
+	
+	--Keep these as player1's name
+	if config.settings.multiplayer_num == 1 then
+	self.save_name = name
+	self.player_name = name
+	end
+	
+	--~~there can be only one~~
+	--if self.party and self.party:findMember{main=true} then
+	--	self.party:findMember{main=true}.name = name
+	--end
+	--there can be more than one
+	if game.player then --remove superfluous error. This really shouldn't be called that early.
+	game.player.name = name
+	end
+end
+
 
 --]]
 return _M

init.lua
@@ -25,7 +25,7 @@ Game rebalance (optional):
 		duplicates quest rewards
 	disallows dropping items?
 transmog popup for both people
-bring both back to life after eidolon
+bring both back to life after eidolon - go there immediately on any player death?
 don't interrupt autoexplore/run (let it stack)
 fix player1 living player2 undead error]]
 tags = {'multiplayer'}

commit c1796385f5d82b0f4aec074d6b8c793442283e73

Fixed loading savegames without naming the save playerthesecond instead of playerthefirst without naming the characters playerthesecond playerthefirst2 without throwing an error without opening a birther dialog on load fixes #20 broken since commit 0a6a041bbc6a7b10a41c0efd4bdb34c1ba91e008

superload/mod/class/Game.lua
@@ -315,24 +315,26 @@ function _M:newGame()
 	self:registerDialog(birth)
 end
 
+--AFFECTS SAVE/LOAD/BIRTH/NEWGAME!
 local base_setPlayerName = _M.setPlayerName
 --- Sets the player name
 function _M:setPlayerName(name)
 	name = name:removeColorCodes():gsub("#", " "):sub(1, 25)
 	
 	--Keep these as player1's name
-	if config.settings.multiplayer_num == 1 then
+	if (not config.settings.multiplayer_num) or config.settings.multiplayer_num <= 1 then
 	self.save_name = name
 	self.player_name = name
 	end
 	
-	--~~there can be only one~~
-	--if self.party and self.party:findMember{main=true} then
-	--	self.party:findMember{main=true}.name = name
-	--end
 	--there can be more than one
 	if game.player then --remove superfluous error. This really shouldn't be called that early.
-	game.player.name = name
+		game.player.name = name
+		else
+		--~~there can be only one~~
+		if self.party and self.party:findMember{main=true} then
+			self.party:findMember{main=true}.name = name
+		end
 	end
 end
 