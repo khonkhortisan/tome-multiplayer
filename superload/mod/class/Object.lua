e688bcc2f8beb741457815053b86f92e46e59d34
Kyle Mills <khonkhortisan@gmail.com>
2018-10-03 15:03:39 -0700

    let player2 pick up quest items fixes #6

---
+++ superload/mod/class/Object.lua
@@ -0,0 +1,20 @@
+local _M = loadPrevious(...)
+local base_on_prepickup = _M.on_prepickup
+
+--- Called when trying to pickup
+function _M:on_prepickup(who, idx)
+	--if self.quest and who ~= game.party:findMember{main=true} then
+	if self.quest and not game.party.members[who].main then
+		return "skip"
+	end
+	if who.player and self.lore then
+		game.level.map:removeObject(who.x, who.y, idx)
+		game.party:learnLore(self.lore)
+		return true
+	end
+	if who.player and self.force_lore_artifact then
+		game.party:additionalLore(self.unique, self:getName(), "artifacts", self.desc)
+		game.party:learnLore(self.unique)
+	end
+end
+return _M
