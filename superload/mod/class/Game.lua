bd7b3df234b9396876b9135cce25b836aa7a7c8c
Kyle Mills <khonkhortisan@gmail.com>
2018-10-03 12:39:42 -0700

    reduce redundancy with similar players

--- superload/mod/class/Game.lua
+++ superload/mod/class/Game.lua
@@ -228,11 +228,39 @@ Welcome #LIGHT_GREEN#]]..self.player.name..[[#LAST#. #LIGHT_BLUE#This is the int
 					if not loaded then --needed to not lock loading saves
 						local player_list = "Players: "
 						local player_num = 1
+						local same_race, same_subclass, are_same_race, are_same_subclass = "", "", true, true
 						for act, _ in pairs(game.party.members) do
 							if game.party.members[act].main then
 								if player_num > 1 then player_list = player_list..", " end
 								player_list = player_list..act.descriptor.race.." "..act.descriptor.subclass.." #LIGHT_GREEN#"..act.name.."#LAST#"
 								player_num = player_num+1
+								if act.descriptor.race ~= same_race then
+									if same_race == "" then
+										same_race = act.descriptor.race
+									else
+										are_same_race = false
+									end
+								end
+								if act.descriptor.subclass ~= same_subclass then
+									if same_subclass == "" then
+										same_subclass = act.descriptor.subclass
+									else
+										are_same_subclass = false
+									end
+								end
+							end
+						end
+						if config.settings.multiplayer_num > 1 then
+							if are_same_subclass or are_same_race then
+								player_list = player_list:gsub("Players: ", "players: ")
+							end
+							if are_same_subclass then
+								player_list = player_list:gsub(same_subclass.." ", "")
+								player_list = same_subclass.." "..player_list
+							end
+							if are_same_race then
+								player_list = player_list:gsub(same_race.." ", "")
+								player_list = same_race.." "..player_list
 							end
 						end
 						player_list = player_list.."."
