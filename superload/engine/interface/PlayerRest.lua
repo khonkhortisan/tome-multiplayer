a8dc5429238e896371ad0d42282577fe377e1c00
Kyle Mills <khonkhortisan@gmail.com>
2018-10-03 16:11:17 -0700

    don't continue resting/running after switching control to another player and back after stopping for a valid reason (other than switching control)

---
+++ superload/engine/interface/PlayerRest.lua
@@ -0,0 +1,20 @@
+local _M = loadPrevious(...)
+
+local base_restStop = _M.restStop
+--local base_runStop = _M.runStop
+
+function _M:restStop(msg)
+	if msg ~= "Switching control" then
+		self.resting_continue=false
+	end
+	base_restStop(self, msg)
+end
+--[[
+function _M:runStop(msg)
+	if msg ~= "Switching control" then
+		self.running_continue=false
+	end
+	base_runStop(self, msg)
+end
+--]]
+return _M
